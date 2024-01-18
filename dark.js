2ad32b356b98389282d1bde47523f9fc2b34cce3
Laura Dumitru <lauradumitru88@icloud.com>
2024-01-18 14:42:23 +0000

    Fixed navbar issue on dark mode

--- dark.js
+++ dark.js
@@ -1,17 +1,20 @@
 let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 const icon = document.querySelector("#dark-mode-toggle i");
+const navbar = document.querySelector(".navbar");
 
 const enableDarkMode = () => {
   document.body.classList.add("darkmode");
   localStorage.setItem("darkMode", "enabled");
   icon.className = "fas fa-sun";
+  navbar.className = "navbar navbar-expand-lg navbar-dark";
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("darkmode");
   localStorage.removeItem("darkMode");
   icon.className = "fas fa-moon";
+  navbar.className = "navbar navbar-expand-lg navbar-light";
 };
 
 if (darkMode === "enabled") {
